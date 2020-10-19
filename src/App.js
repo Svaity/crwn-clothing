d755c63c5701f10ea4b160b34905a0284cba6d53
shrey vaity <vaityshrey@gmail.com>
2020-10-19 10:34:46 -0400

    update menu item navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.components'
 
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route path="/hats" component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
